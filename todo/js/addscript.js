f4f4cdd882fc15d4058fcc6f61969f349a9945a4
Natallia Perets <n.perets@intetics.com>
2014-07-20 01:18:49 +0300

    poor css added

--- todo/js/addscript.js
+++ todo/js/addscript.js
@@ -1,29 +1,21 @@
-function append(form) {
-   if (form.input.value) {
-      var newItem = document.createElement("LI");
+function append() {
+   if (input.value) {
+      var newItem = document.createElement("LI")
+      newItem.id = "item"
       var newText = document.createElement("DIV")
-      newText.innerHTML = form.input.value
+      newText.innerHTML = input.value
       var newButton = document.createElement("BUTTON")
-      newButton.class = "delete"
-      newButton.innerHTML = 'delete'
+      newButton.id = "delete"
       newItem.appendChild(newText);
       newItem.appendChild(newButton);
-      document.getElementById("list").appendChild(newItem);
+      document.getElementById("list").appendChild(newItem)
       document.getElementById("list").onclick = function(e) {
-    e = e || window.event   
-    var target = e.target || e.srcElement 
-
-    if (target.nodeName != 'BUTTON') return
-
-    //target.style.visibility = 'hidden'
-	var childr = target.parentNode
-	target.parentNode.parentNode.removeChild(childr)
-	
-	/*for(ind = 0; ind < childr.length; ++ind){
-		alert(childr[ind].nodeName)
-		target.parentNode.removeChild(childr[ind])
-	}*/
-    return false // prevent url change
-  }
-  }
+    	e = e || window.event   
+    	var target = e.target || e.srcElement 
+		if (target.nodeName != 'BUTTON') return
+		var childr = target.parentNode
+		target.parentNode.parentNode.removeChild(childr)
+  		}
+  	  document.getElementById("input").value = ""
+    }
 }
